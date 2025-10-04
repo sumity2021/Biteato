52495157c54ecb7ecd5ce0abf6c3833e89be3285
sumity2021 <sumity2021@gmail.com>
2025-10-04 18:06:03 +0530

    cors fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+// create server
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
@@ -6,28 +7,12 @@ const foodPartnerRoutes = require("./routes/food-partner.routes");
 const cors = require("cors");
 
 const app = express();
-
-// Frontend URLs
-const allowedOrigins = [
-  process.env.FRONTEND_URL, // deployed frontend
-  "http://localhost:5173", // local dev
-];
-
-// CORS middleware
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
-
 app.use(cookieParser());
 app.use(express.json());
 
