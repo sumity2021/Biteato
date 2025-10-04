841a11f605c052a2408cd5678ab2cf328b668866
sumity2021 <sumity2021@gmail.com>
2025-10-04 15:30:31 +0530

    password reset

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -1,27 +1,24 @@
 const nodemailer = require("nodemailer");
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendResetEmail(to, resetToken) {
-  const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-  const msg = {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
     to: to,
-    from: process.env.EMAIL_FROM,
     subject: "Password Reset",
-    html: `<p>You requested a password reset</p>
-           <p>Click <a href="${resetLink}">here</a> to reset your password</p>
-           <p>This link will expire in 10 minutes.</p>`,
+    html: `<p>You requested a password reset</p><p>Click <a href="${process.env.FRONTEND_URL}/reset-password?token=${resetToken}">here</a> to reset your password</p> <p>This link will expire in 10 minutes.</p>`,
   };
 
-  try {
-    await sgMail.send(msg);
-    console.log("Reset email sent to", to);
-  } catch (err) {
-    console.error("SendGrid error:", err);
-    throw err;
-  }
+  await transporter.sendMail(mailOptions);
 }
 
 module.exports = { sendResetEmail };
